8735a6dd558245c423cb411157501c161a296f3e
Lukas <brauckma@hm.edu>
2015-11-03 15:27:24 -0500

    Added simple Python receiver

---
+++ src/receiver.py
@@ -0,0 +1,20 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Lukas"
+__date__ = "$03.11.2015 15:15:40$"
+
+import socket
+
+SERVER_ADDRESS = ('localhost', 10000)
+BUFFER_SIZE = 1024
+MESSAGE = "Success!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(SERVER_ADDRESS)
+data = s.recv(BUFFER_SIZE)
+s.send(MESSAGE)
+s.close()
+
+print "received data:", data
