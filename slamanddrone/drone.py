4c7834b26da530fe5ebe16df6718a80193c1da09
RaspPiMQP <gjvincentelli@wpi.edu>
2015-11-12 19:06:55 +0000

    slam with dorne

---
+++ slamanddrone/drone.py
@@ -0,0 +1,187 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "lukas"
+__date__ = "$10.11.2015 10:12:49$"
+
+import libardrone
+import cv2
+import time
+from vehicle import Vehicle
+import threading
+
+class Drone(Vehicle,threading.Thread):
+    """
+    Class representing a connection to the ARDrone, controls it and receive navdata information
+    """
+
+    def __init__(self):
+        """
+        Initialize the connection and variables
+        """
+        threading.Thread.__init__(self)
+        print "Connecting..."
+        self.cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+        self.drone = libardrone.ARDrone()
+        print "Ok."
+        cv2.namedWindow('Front camera')
+        # Counter for file names
+        self.frame_count=1
+        self.running = True
+        self.first = True
+        # 'Traveled' distance in mm
+        self.distance_frame = 0.0
+        self.psi_update = self.drone.navdata.get(0, dict()).get('psi', 0)
+        self.timestamp_frame = 0.0
+        self.timestamp_update = 0.0
+        self.start()
+
+    def get_information(self): 
+        """
+        Receive navdata information and set them on the frame
+        """        
+        self.calc_distance(self.drone.navdata.get(0, dict()).get('vx', 0))
+
+        # creating battery information string
+        battery_str = "Battery: "
+        battery_str += str(self.drone.navdata.get(0, dict()).get('battery', 0))
+        battery_str += "%"
+        cv2.putText(self.frame, battery_str, (10,350), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
+        
+    def save_frame(self):
+        """
+        Save current frame
+        """
+        file = "Frames/#"
+        file += str(self.frame_count)
+        file += ".jpg"
+        cv2.imwrite(file,self.frame)
+        self.frame_count += 1
+        print "Frame saved"
+        
+    def update(self, psi):
+        # distance
+        dx = self.distance_frame
+        self.distance_frame = 0.0
+        return dx,self.calc_psi(psi),self.get_dt_update()
+        
+    def calc_dt(self,old):
+        """
+        Return the time difference between old time and now
+        """
+        now = time.time()
+        if old == 0.0:
+            return 0.0,now
+        dt = now-old
+        return dt, now
+    
+    def get_dt_frame(self):
+        """
+        Return the time difference between since the last frame
+        """
+        dt,self.timestamp_frame=self.calc_dt(self.timestamp_frame)
+        return dt
+    
+    def get_dt_update(self):
+        """
+        Return the time difference between since the last update
+        """
+        dt,self.timestamp_update=self.calc_dt(self.timestamp_update)
+        return dt
+        
+    def calc_distance(self, vx):
+        """
+        Calculate distance since last frame
+        """
+        if vx < 0.0:
+            vx = 0.0
+        self.distance_frame += (vx*self.get_dt_frame())
+        if self.distance_frame < 0.0:
+            self.distance_frame = 0.0
+        else:
+            self.distance_frame=round(self.distance_frame, 0)
+            
+    def calc_psi(self, psi):
+        """
+        Calculate psi since last call
+        """
+        dd = psi - self.psi_update
+        self.psi_update = psi
+        return dd
+        
+    def getOdometry(self):
+        """
+        return a tuple of odometry (dxy in mm,dthata in degree, dt in s)
+        """
+        if self.first:
+            self.first = False
+            return 0.0,0,0.0
+        return self.update(self.drone.navdata.get(0, dict()).get('psi', 0))    
+        
+    def run(self):
+        """
+        Main loop
+        """
+        while self.running:
+            # get current frame of video
+            self.running, self.frame = self.cam.read()
+            self.get_information()
+            if self.running:     
+                # show current frame
+                cv2.imshow('Front camera', self.frame)
+                k = cv2.waitKey(1)
+                if k==27:   # Esc
+                    self.running = False
+                    self.shutdown()
+                elif k==10:  # Return
+                    self.drone.takeoff()
+                    print "Return pressed"
+                elif k==32:  # Space
+                    self.drone.land()
+                    print "Space pressed"
+                elif k==119:  # w
+                    self.drone.move_forward()
+                    print "W pressed"
+                elif k==97:  # a
+                    self.drone.move_left()
+                    print "A pressed"
+                elif k==115:  # s
+                    self.drone.move_backward()
+                    print "S pressed"
+                elif k==100:  # d
+                    self.drone.move_right()
+                    print "D pressed"
+                elif k==65362:  # Up
+                    self.drone.move_up()
+                    print "Up pressed"
+                elif k==65364:  # Down
+                    self.drone.move_down()
+                    print "Down pressed"
+                elif k==65361:  # Left
+                    self.drone.turn_left()
+                    print "Left pressed"
+                elif k==65363:  # Right
+                    self.drone.turn_right()
+                    print "Right pressed"
+                elif k==112:  # p
+                    self.save_frame()
+                elif k==117:  # u
+                    print self.getOdometry()
+                elif k==-1:  # other
+                    continue
+                else:
+                    print k
+            else:
+                # error reading frame
+                print "Error reading video feed"
+                
+    def shutdown(self):
+        """
+        Close application
+        """
+        print "Shutting down..."
+        self.cam.release()
+        cv2.destroyAllWindows()
+        self.drone.halt()
+        print "Ok."
---
+++ slamanddrone/sensor.py
@@ -0,0 +1,161 @@
+'''
+sensor.py : Asus xtion which emulates a laser scaner
+             
+author: Nils Bernhardt 
+'''
+
+from breezyslam.components import Laser
+from reader import Reader
+
+import math
+
+
+
+
+
+class XTION(Laser):
+
+    viewangle = 58 #asus xtion view in degrees
+    linecount = 7 #lines above and below to generate average (0=online desired line)
+    distance_no_detection_mm = 10000 # value used if sensor detects 0
+    scan_rate_hz = 1 #todo find value
+    detectionMargin = 5 #pixels on the sites of the scans which should be ignored
+    offsetMillimeters = 0 #offset of the sensor to the center of the robot
+    
+    '''
+    A class for the Asus XTION
+    '''
+    def __init__(self, log = True):
+        self.log = log
+        if(log):
+            self.out = open('log', 'w')
+        self.reader = Reader()
+        self.width = self.reader.getHeight()
+        self.height = self.reader.getWidth()
+        self.row = self.height/2 #row to read
+        Laser.__init__(self, self.width, self.scan_rate_hz, self.viewangle, self.distance_no_detection_mm, self.detectionMargin, self.offsetMillimeters)
+        
+    
+
+    '''
+    Scans one line
+    return: array with the values
+    '''
+    def scan(self):
+        frame = self.reader.readFrame()
+        data = self.readLine(frame, self.width, self.height, self.row)
+        return data
+
+        
+    '''
+    #Prints the depth value for every pixel in one line
+    #frame_data - depth frame
+    #width -  width of the frame
+    #height - heigth of the frame
+    #line - line to print
+    return one data row converted as lidar
+    '''
+    def readLine(self, frame_data, width, height, line):
+        data = []
+        for x in range(width-1, -1, -1):
+            value = self.getAverageDepth(frame_data, width, height, x, line, self.linecount)
+            converted = self.toLidarValue(value, x, width)
+            if(self.log):
+                self.out.write(str(converted) + ' ')
+            data.append(converted)
+        if(self.log):
+            self.out.write('\n')
+        return data
+
+    '''
+    Converts the measured value of the asus xtion to the value a lidar would measure
+    value: value to convert
+    x: x position of the value
+    width: of the frame
+    return: converted value
+    '''
+    def toLidarValue(self, value, x, width):
+        angle = (float(width)/2-x)/width*self.viewangle
+        return int(value/math.cos(math.radians(angle)))
+
+    '''
+    #get the average value of a specifiv pixel with a certain amount of pixel above and under.
+    #frame_data - depth frame
+    #widht -  width of the frame
+    #height - height of the frame
+    #x - coordinate of the pixel
+    #y - coordinate of the pixel
+    #distance - pixels under and above the desired row
+    return: average value
+    '''
+    def getAverageDepth (self, frame_data, width, height, x, y, distance):
+        sum = 0;
+        count = 0
+        for xTemp in range (-distance+x, distance+1+x):            
+            value = frame_data[y*width+xTemp]
+            if(value>0):
+                sum += value
+                count += 1
+        if(count>0):
+            return sum/count
+        else:
+            return 0
+
+class FileXTION(XTION):
+    #current frame read
+    index = 0
+
+    '''
+    A class for reading the log file of an Asus XTION
+    
+    dataset: filename
+    datadir: directionary of the file default '.'
+    '''
+    def __init__(self, dataset, datadir = '.'):
+        self.scans, width = self.load_data(datadir, dataset)
+        Laser.__init__(self, width, self.scan_rate_hz, self.viewangle, self.distance_no_detection_mm, self.detectionMargin, self.offsetMillimeters)
+
+    '''
+    reads a scan 
+    return: array with the values
+    '''
+    def scan(self):
+        if(self.index < len(self.scans)):
+           self.index += 1
+           return self.scans[self.index-1]
+        else:
+           return []
+
+    '''
+    loads a stroed log file and saves the scans.
+    datadir: directionary of the file
+    dataset: filename
+    return: scans, width of the scans
+    '''
+    def load_data(self, datadir, dataset):
+        
+        filename = '%s/%s' % (datadir, dataset)
+        print('Loading data from %s...' % filename)
+        
+        fd = open(filename, 'rt')
+        
+        scans = []
+        
+        while True:  
+            
+            s = fd.readline()
+            
+            if len(s) == 0:
+                break       
+                
+            toks = s.split()[0:-1] # ignore ''
+                            
+            lidar = [int(tok) for tok in toks[:]]
+            
+
+            scans.append(lidar)
+            
+        fd.close()
+            
+        return scans, len(scans[0])
+        
