6c3efc3388859e1b38e7aedfe61ff59cfe8645f4
Lukas <lukas.brauckmann@gmail.com>
2015-11-13 14:21:45 -0500

    Bugfixes and improvements

--- breezyslam/drone.py
+++ breezyslam/drone.py
@@ -8,11 +8,11 @@ __date__ = "$10.11.2015 10:12:49$"
 import sys
 sys.path.insert(0, '/home/pi/libardrone/python-ardrone')
 import libardrone
-
 import cv2
 import time
-from vehicle import Vehicle
 import threading
+import math
+from vehicle import Vehicle
 
 class Drone(Vehicle,threading.Thread):
     """
@@ -20,8 +20,7 @@ class Drone(Vehicle,threading.Thread):
     """
     running = True
     correct_psi = True
-    timestamp_frame = 0.0
-    timestamp_update = 0.0
+    old_timestamp = 0.0
 
     def __init__(self):
         """
@@ -35,35 +34,22 @@ class Drone(Vehicle,threading.Thread):
         self.last_thata = self.drone.navdata.get(0, dict()).get('psi', 0)
         #cv2.namedWindow('Front camera')
     
-    def get_dt_frame(self):
-        """
-        Return the time difference between since the last frame
-        """
-        dt,self.timestamp_frame=self.calc_dt(self.timestamp_frame)
-        return dt
-    
-    def get_dt_update(self):
+    def get_dt(self):
         """
         Return the time difference between since the last update
         """
-        dt,self.timestamp_update=self.calc_dt(self.timestamp_update)
+	now = time.time()
+        if old_timestamp == 0.0:
+            return 0.0
+        dt = now-old_timestamp
+        old_timestamp = now
         return dt
-
-    def calc_dt(self,old):
-        """
-        Return the time difference between old time and now
-        """
-        now = time.time()
-        if old == 0.0:
-            return 0.0,now
-        dt = now-old
-        return dt, now
         
-    def calc_distance(self, vx):
+    def calc_distance(self, vx, dt):
         """
         Calculate distance since last frame
         """
-        return vx*self.get_dt_frame()
+        return vx*dt
             
     def calc_dthata(self, thata):
         """
@@ -77,11 +63,12 @@ class Drone(Vehicle,threading.Thread):
         """
         return a tuple of odometry (dxy in mm,dthata in degree, dt in s)
         """
+	dt = get_dt()
 	dthata = self.calc_dthata(self.drone.navdata.get(0, dict()).get('psi', 0))
-        if self.correct_psi & dthata > 20:
+        if self.correct_psi & math.fabs(dthata) > 20:
 		dthata = 0
 		self.correct_psi = False	
-        return self.calc_distance(self.drone.navdata.get(0, dict()).get('vx', 0)), dthata, self.get_dt_update()
+        return self.calc_distance(self.drone.navdata.get(0, dict()).get('vx', 0), dt), dthata, dt
         
     def initialize(self):
         """
