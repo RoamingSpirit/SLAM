f4ce6e1696ad90d4354cc3258ed3f44ae2a3c824
thhughes <Troy.h.hughes@gmail.com>
2015-11-18 10:24:10 -0500

    Updated files

--- breezyslam/CPI_Protocol/SCommunicator.py
+++ breezyslam/CPI_Protocol/SCommunicator.py
@@ -1,11 +1,16 @@
 __author__ = 'Troy Hughes'
 
 import serial
+from serial import SerialTimeoutException
 
 
 class SCommunicator(object):
     def __init__(self,COMPort):
-        self._s = serial.Serial(COMPort, baudrate=115200)
+        self._s = serial.Serial(COMPort,
+                                baudrate=115200,
+                                timeout = 3,
+                                write_timeout = 0.1)
+        self._port = COMPort
         self._s.open()
 
 
@@ -13,7 +18,36 @@ class SCommunicator(object):
         self._s.close()
 
 
-""" From here we need to define: GET and GIVE commands
-GET: Master send a specific command to the slave and wait for a response, timeout after X time.
-GIVE: Master will send a value to the slave and will not wait for a response.
-"""
+    """ From here we need to define: GET and GIVE commands
+    GET: Master send a specific command to the slave and wait for a response, timeout after X time.
+    GIVE: Master will send a value to the slave and will not wait for a response.
+    """
+
+    def _safeSend(self, data, location):
+        try: self._s.write(self,data)
+        except SerialTimeoutException,e:
+            print "Your error location is: "+location
+            raise SerialTimeoutException("SCommunicatory on port: "+self._port+" has failed to write")
+
+    def _safeRecv(self, size):
+        data = self._s.read()
+
+    def get(self,commandType, commandData):
+
+        sendCommand = commandType+"::"+commandData
+
+        self._safeSend(sendCommand, "SCommunicator.get")
+
+
+
+
+
+
+
+    def ack(self):
+        
+        # try:self._s.write("|")
+        #
+        return
+
+
