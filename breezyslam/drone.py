f7f4e20ab0729204fe7c19c3f8fe0712e3f73427
Lukas <brauckma@hm.edu>
2015-11-19 12:50:32 -0500

    Added move commands for the drone. Added controller class, to control drone via gamepad.

--- breezyslam/drone.py
+++ breezyslam/drone.py
@@ -18,8 +18,12 @@ class Drone(Vehicle):
     Class representing a connection to the ARDrone,
     controls it and receive navdata information
     """
-    running = True
     correct_psi = True
+    move = False
+    turn = False
+    in_air = False
+    distance = 0
+    angle = 0
     old_timestamp = 0.0
 
     def __init__(self, log = True):
@@ -32,9 +36,9 @@ class Drone(Vehicle):
         print "Connecting..."
         self.cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
         self.drone = libardrone.ARDrone()
+        self.drone.set_speed(0.1)
         print "Ok."
         self.last_thata = self.drone.navdata.get(0, dict()).get('psi', 0)
-        #cv2.namedWindow('Front camera')
 
     def get_dt(self):
         """
@@ -52,7 +56,10 @@ class Drone(Vehicle):
         """
         Calculate distance since last frame
         """
-        return vx*dt
+        dx = vx*dt
+        if move:
+            distance -= dx
+        return dx
 
     def calc_dthata(self, thata):
         """
@@ -60,12 +67,32 @@ class Drone(Vehicle):
         """
         dthata = thata - self.last_thata
         self.last_thata = thata
+        if turn:
+            angle -= dthata
         return dthata
 
     def getOdometry(self):
         """
-        return a tuple of odometry (dxy in mm,dthata in degree, dt in s)
+        Return a tuple of odometry (dxy in mm,dthata in degree, dt in s)
         """
+		# Move the drone
+        if self.distance > 0 & self.move:
+            self.drone.move_forward()
+        else:
+            self.move = False
+            self.distance = 0
+            self.drone.hover()
+		
+		# Turn the drone
+        if self.angle < 0 & self.turn:
+            self.drone.turn_left()
+        elif self.angle > 0 & self.turn:
+            self.drone.turn_right()
+        else:
+            self.turn = False
+            self.drone.hover()
+		
+		# Get odometry data
         dt = self.get_dt()
         dthata = self.calc_dthata(self.drone.navdata.get(0, dict()).get('psi', 0))
         if self.correct_psi & (math.fabs(dthata) > 20):
@@ -75,22 +102,37 @@ class Drone(Vehicle):
         if(self.log):
                 self.out.write("%f %f %f\n" % data)
         return data
+		
+	def move(self, dxy):
+		"""
+		Set the distance to move forward
+		"""
+		self.move = True
+		self.distance = dxy
+	
+	def turn(self, dtheta):
+		"""
+		Set the turn angle
+		"""
+		self.turn = True
+		self.angle = dtheta
         
     def initialize(self):
         """
-        Lets the drone fly
+        Let the drone fly
         """
         print "Take off"
-        #self.drone.takeoff()
-	print "Drone in air!"
+        self.drone.takeoff()
+	    print "Drone in air!"
 
     def shutdown(self):
         """
         Close application
         """
         print "Shutting down..."
-        #self.drone.land()
+        self.drone.land()
+		self.move = False
+		self.turn = False
         self.cam.release()
-        self.running = False
         self.drone.halt()
         print "Drone shutted down."
