7de814e6d783d7fed7753dfbf5385a47e3ad2623
RaspPiMQP <gjvincentelli@wpi.edu>
2015-11-19 17:56:05 +0000

    drone test program

---
+++ breezyslam/dronetest.py
@@ -0,0 +1,19 @@
+from drone import Drone
+import time
+
+
+try:
+    drone = Drone()
+    drone.initialize()
+    drone.turn(90)
+    time.sleep(2)
+    drone.turn(-90)
+    time.sleep(2)
+    drone.turn(180)
+    time.sleep(2)
+    drone.move(500)
+    time.sleep(2)
+    drone.shutdown()
+except KeyboardInterrupt:
+    print('\n\nKeyboard exception received. Emergency shutdown.')
+    drone.shutdown()
