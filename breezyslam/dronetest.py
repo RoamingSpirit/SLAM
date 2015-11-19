57e08fc842fcd6ad15ec3823c548a7785caccf44
RaspPiMQP <gjvincentelli@wpi.edu>
2015-11-19 18:34:50 +0000

    dronetest

--- breezyslam/dronetest.py
+++ breezyslam/dronetest.py
@@ -6,13 +6,12 @@ try:
     drone = Drone()
     drone.initialize()
     time.sleep(5)
+    print 'rotate'
     drone.turn(90)
     time.sleep(2)
     drone.turn(-90)
     time.sleep(2)
     drone.turn(180)
-    time.sleep(2)
-    drone.move(500)
     time.sleep(2)
     drone.shutdown()
 except KeyboardInterrupt:
