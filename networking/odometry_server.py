5eae4d94700658d0a58d0fea0ef11df63c5e0042
Lukas <lukas.brauckmann@gmail.com>
2015-11-20 17:51:08 -0500

    Added clietn and server for turtlebot communication.

---
+++ networking/odometry_server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+
+HOST = ""
+PORT = 8888
+
+class Odometry_Server(threading.Thread):
+	
+	running = True
+	
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.start()
+
+	def run(self):
+		"""
+		Main loop
+		"""
+		self.setup()
+        
+		while self.running:
+			msg = self.connection.recv(1024)
+			if msg == "odometry":
+				self.connection.send("0.1,1,1")
+           
+	def setup(self):
+		'''
+		Setup a connection to a client on PORT.
+		'''
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print 'Socket created'
+         
+        # Bind socket to local host and port
+		try:
+			self.socket.bind((HOST, PORT))
+		except socket.error as msg:
+			print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+			self.running = False
+            
+		if self.running:     
+			print 'Socket bind complete'
+             
+            # Start listening on socket
+			self.socket.listen(1)
+			print 'Socket now listening'
+             
+            # Connect to the client
+			try:
+				self.connection, addr = self.socket.accept()
+				print 'Connected with ' + addr[0] + ':' + str(addr[1])
+			except socket.error, e:
+				self.close()
+				print 'Socket closed'
+            
+	def close(self):
+		'''
+		Close the connection and stop the running thread.
+		'''
+		self.running = False
+		self.socket.shutdown(socket.SHUT_RDWR)
+		self.socket.close()
+
+if __name__ == '__main__':
+	server = Odometry_Server()
+
