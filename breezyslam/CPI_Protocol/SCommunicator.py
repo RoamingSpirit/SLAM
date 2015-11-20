ea8175253e77f3bcd3e486c1aea9e8ff8f115d54
thhughes <Troy.h.hughes@gmail.com>
2015-11-19 23:23:40 -0500

    Updated SCommunicator

--- breezyslam/CPI_Protocol/SCommunicator.py
+++ breezyslam/CPI_Protocol/SCommunicator.py
@@ -2,7 +2,7 @@ __author__ = 'Troy Hughes'
 
 import serial
 from serial import SerialTimeoutException
-
+import time
 
 class SCommunicator(object):
     def __init__(self,COMPort):
@@ -24,30 +24,61 @@ class SCommunicator(object):
     """
 
     def _safeSend(self, data, location):
-        try: self._s.write(self,data)
+        try: self._send(data)
         except SerialTimeoutException,e:
             print "Your error location is: "+location
             raise SerialTimeoutException("SCommunicatory on port: "+self._port+" has failed to write")
 
-    def _safeRecv(self, size):
-        data = self._s.read()
+    def _safeRecv(self, size, timeout=1, size_known=True):
+        try: data = self._recv(size, timeout, size_known)
+        except RuntimeError,e:
+            raise e
+        return data
 
-    def get(self,commandType, commandData):
-
+    def getData(self,commandType, commandData):
         sendCommand = commandType+"::"+commandData
+        self._safeSend(sendCommand, "SCommunicator.getData")
 
-        self._safeSend(sendCommand, "SCommunicator.get")
+        if not self.recvAck():
+            raise RuntimeError("Ack Dropped from getData Function")
+        data = self._safeRecv(None,None,False)
+        self.ack()
+        return
 
+    def sendData(self,data):
+        sData = str(data)
+        sData = sData + '\r\n'
 
-
-
+        self._safeSend(sData,'SendData')
+        return
 
 
 
     def ack(self):
-        
-        # try:self._s.write("|")
-        #
+        self._safeSend('!',"ACK_Send")
+
+    def recvAck(self):
+        data = self._safeRecv(1)
+        if data == '!':
+            return True
+        else:
+            return False
+
+
+    def _send(self,value):
+        self._s.write(value)
         return
 
+    def _recv(self,size, timeout, size_known=True):
+        if size_known:
+            start_time = time.time()
+            while ((time.time() - start_time <= timeout) and self._s.inWaiting() < size):
+                continue
+            if time.time() - start_time <= timeout:
+                raise RuntimeError("Timout while trying to _recv")
+            return self._s.read(size)
+        else:
+            data = self._s.readline()
+            print data
+            return data
 
