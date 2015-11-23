111ec7078e86f26db0c679d1d91f36346c7494d3
Lukas <lukas.brauckmann@gmail.com>
2015-11-23 14:26:24 -0500

    Bugfixes.

--- networking/server.py
+++ networking/server.py
@@ -13,51 +13,63 @@ class Server(threading.Thread):
 		self.start()
 
 	def run(self):
-		"""
+		'''
 		Main loop
-		"""
+		'''
 		self.setup()
-        
+		
+		msg = ""
 		while self.running:
-			msg = self.connection.recv(1024)
-			if msg == "odometry":
-				self.connection.send("0.1,1,1")
-			elif msg == "move":
-				dx = self.connection.recv(1024)
-				print dx
-			elif msg == "turn":
-				dthata = self.connection.recv(1024)
-				print dthata
-           
+			while ";" not in msg:
+				msg += self.connection.recv(1024)
+			commands = msg.split(";")
+			if commands[len(commands)-1] == "":
+				for i in range(0, len(commands)-1):
+					detail = commands[i].split(",")
+					if detail[0] == "odometry":
+						# TODO: odometry command
+						self.connection.send("0.1,1,1") # Only testing!
+					elif detail[0] == "move":
+						# TODO: move command
+						dx = detail[1]
+						print "Move by: ", dx
+					elif detail[0] == "turn":
+						# TODO: turn command
+						dthata = detail[1]
+						print "Turn by: ", dthata
+				msg = ""
+			else:
+				msg = commands[len(commands)-1]
+				           
 	def setup(self):
 		'''
 		Setup a connection to a client on PORT.
 		'''
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		print 'Socket created'
-         
-        # Bind socket to local host and port
+		print "Socket created"
+		
+		# Bind socket to local host and port
 		try:
 			self.socket.bind((HOST, PORT))
 		except socket.error as msg:
-			print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+			print "Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1]
 			self.running = False
-            
+			
 		if self.running:     
-			print 'Socket bind complete'
-             
-            # Start listening on socket
+			print "Socket bind complete"
+			
+			# Start listening on socket
 			self.socket.listen(1)
-			print 'Socket now listening'
-             
-            # Connect to the client
+			print "Socket now listening"
+			
+			# Connect to the client
 			try:
 				self.connection, addr = self.socket.accept()
-				print 'Connected with ' + addr[0] + ':' + str(addr[1])
+				print "Connected with " + addr[0] + ":" + str(addr[1])
 			except socket.error, e:
 				self.close()
-				print 'Socket closed'
-            
+				print "Socket closed"
+				
 	def close(self):
 		'''
 		Close the connection and stop the running thread.
@@ -65,6 +77,7 @@ class Server(threading.Thread):
 		self.running = False
 		self.socket.shutdown(socket.SHUT_RDWR)
 		self.socket.close()
+		print "Socket closed"
 
 if __name__ == '__main__':
 	server = Server()
--- networking/turtlebot.py
+++ networking/turtlebot.py
@@ -1,39 +1,61 @@
 import socket
+from vehicle import Vehicle
 
 HOST = ""
 PORT = 8888
 
-class Turtlebot():
-
-	def __init__(self):
-		"""
-		Initialize the client and connect to the server.
-		"""
-		self.socket = socket.socket()          
-		self.socket.connect((HOST, PORT))
-		print "Connected."
+class Turtlebot(Vehicle):
 		
+	def __init__(self):
+		self.socket = socket.socket()
+	
 	def getOdometry(self):
-		self.socket.send("odometry")
+		self.socket.send("odometry;")
 		data = self.socket.recv(1024)
 		tuple = data.split(",", 3)
 		return tuple
 	
 	def close(self):
+		'''
+		Close the connection.
+		'''
 		self.socket.close()
 		
 	def move(self, dx):
-		self.socket.send("move") 
-		self.socket.send(str(dx)) 
+		'''
+		Move the TurtleBot by dx mm.
+		'''
+		self.socket.send("move,") 
+		self.socket.send(str(dx))
+		self.socket.send(";")
 		
-	def turn(self, dthata):
-		self.socket.send("turn") 
-		self.socket.send(str(dthata)) 
+	def turn(self, dtheta):
+		'''
+		Turn the TurtleBot by dtheta degree.
+		'''
+		self.socket.send("turn,") 
+		self.socket.send(str(dtheta))
+		self.socket.send(";")
+		
+	def initialize(self):
+		'''
+		Iinitialize
+		'''          
+		self.socket.connect((HOST, PORT))
+		print "Connected."
+
+	def shutdown(self):
+		'''
+		Close connection
+		'''
+		self.socket.close()
+		print "Connection closed."
 
 if __name__ == '__main__':
 	socket = Turtlebot()
+	socket.initialize()
 	print socket.getOdometry()
-	print socket.move(10)
-	print socket.turn(90)
+	socket.move(10)
+	socket.turn(90)
 	socket.close()
 
