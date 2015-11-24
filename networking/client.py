0a05cc9f83dcf6a056b2fcd0988cd18c0184afa2
Lukas <lukas.brauckmann@gmail.com>
2015-11-24 11:34:45 -0500

    Changed steering commands in NetworkVehicle.

--- networking/client.py
+++ networking/client.py
@@ -1,9 +1,10 @@
 import socket
 import threading
 from vehicle import Vehicle
-from turtlebot import Turtlebot
+#from turtlebot import Turtlebot
 
-HOST = '192.168.1.108'
+#HOST = '192.168.1.108'
+HOST = ""
 PORT = 8888
 
 class Client(threading.Thread):
@@ -22,31 +23,23 @@ class Client(threading.Thread):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((HOST, PORT))
 		
-		msg = ""
 		while self.running:
-			while "\n" not in msg:
-				msg += self.socket.recv(1024)
-			commands = msg.split("\n")
+			msg = self.socket.recv(1)
 
-			for i in range(0, len(commands)-1):
-				detail = commands[i].split(",")
-				if detail[0] == "initialize":
-					self.robot.initialize()
-					print "Initialize."
-				elif detail[0] == "odometry":
-					self.socket.send(str(self.robot.getOdometry())) # Only testing!
-				elif detail[0] == "move":
-					self.robot.move(float(detail[1]))
-					print "Move by: ", detail[1]
-				elif detail[0] == "turn":
-					self.robot.turn(-float(detail[1]))
-					print "Turn by: ", detail[1]
-				elif detail[0] == "close":
-					self.close()
-					
-				msg = commands[len(commands)-1]
-				           
-	
+			if msg == "0":
+				#self.robot.initialize()
+				print "Initialize"
+			elif msg == "1":
+				self.close()
+			elif msg == "2":
+				#self.robot.move()
+				print "Move forward"
+			elif msg == "3":
+				#self.robot.turn(1)
+				print "Turn right"
+			elif msg == "4":
+				#self.robot.turn(-1)
+				print "Turn left"
 				
 	def close(self):
 		'''
@@ -58,7 +51,7 @@ class Client(threading.Thread):
 		print "Socket closed"
 
 if __name__ == '__main__':
-        testBot = Turtlebot()
+        testBot = Vehicle()
         client = Client(testBot)
         
 
