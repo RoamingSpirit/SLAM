f47f776dd95193035b5b21fe0729ef6105dd2999
Lukas <lukas.brauckmann@gmail.com>
2015-11-25 13:42:57 -0500

    Fix receiving bytes.

--- drone/droneclient.py
+++ drone/droneclient.py
@@ -2,56 +2,58 @@ import socket
 import threading
 #from drone import Drone
 
-HOST = ""
-PORT = 9000
-
 class DroneClient(threading.Thread):
 
     running = True
 
-    def __init__(self, host = ""):
+    def __init__(self, host = "", port = 9000):
         threading.Thread.__init__(self)
-        HOST = host
+        self.host = host
+        self.port = port
         self.start()
 
     def run(self):
         '''
         Main loop
         '''
+        print "Connecting.."
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((HOST, PORT))
+        self.socket.connect((self.host, self.port))
+        print "Connected to host."
         
         while self.running:
-            msg = self.socket.recv(1)
-            if msg == "":
-                pass
-            else:
-                print msg
-            if msg == "0":
-                self.drone = Drone()
-                print "Initialize"
-                self.drone.initialize()
-                self.socket.send("1")
-            elif msg == "1":
-                self.drone.shutdown()
+            try:
+                msg = self.socket.recv(1)
+                if msg == chr(0):
+                    self.drone = Drone()
+                    print "Initialize."
+                    self.drone.initialize()
+                    self.socket.send(chr(1))
+                elif msg == chr(1):
+                    print "Shutdown"
+                    self.drone.shutdown()
+                    self.close()
+                elif msg == chr(2):
+                    print "Move forward."
+                    self.drone.move(msg)
+                    self.socket.send(self.drone.getOdometry())
+                elif msg == chr(3):
+                    print "Turn right."
+                    self.drone.move(msg)
+                    self.socket.send(self.drone.getOdometry())
+                elif msg == chr(4):
+                    print "Turn left."
+                    self.drone.move(msg)
+                    self.socket.send(self.drone.getOdometry())
+                elif msg == chr(5):
+                    print "Stagnate."
+                    self.socket.send(self.drone.getOdometry())
+                    
+            except socket.error, e:
+                print "Error."
                 self.close()
-            elif msg == "2":
-                print "Move forward"
-                self.drone.move(msg)
-                self.socket.send(self.drone.getOdometry())
-            elif msg == "3":
-                print "Turn right"
-                self.drone.move(msg)
-                self.socket.send(self.drone.getOdometry())
-            elif msg == "4":
-                print "Turn left"
-                self.drone.move(msg)
-                self.socket.send(self.drone.getOdometry())
-            elif msg == "4":
-                print "Stagnate"
-                self.socket.send(self.drone.getOdometry())
-                
-            if self.kbhit():
+            except KeyboardInterrupt:
+                print "Interrupted."
                 self.close()
                 
     def close(self):
@@ -61,11 +63,7 @@ class DroneClient(threading.Thread):
         self.running = False
         self.drone.shutdown()
         self.socket.close()
-        print "Socket closed"
-        
-    def kbhit():
-        dr,dw,de = select([sys.stdin], [], [], 0)
-        return dr <> []
+        print "Socket closed."
 
 if __name__ == '__main__':
     try:
