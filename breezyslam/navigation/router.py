8fd39eb97d185c8f952f1e41c3fe192e79f0e0ce
RaspPiMQP <gjvincentelli@wpi.edu>
2015-12-02 22:31:10 +0000

    Routing

--- breezyslam/navigation/router.py
+++ breezyslam/navigation/router.py
@@ -8,10 +8,10 @@ author: Nils Bernhardt
 class Router():
 
     
-    def __init__(self):
+    def __init__(self, MAP_SIZE_PIXELS, MAP_SIZE_METERS, ROBOT_SIZE_METERS, offset_in_scan, min_distance):
         return
     
-    def getRoute(self):
+    def getRoute(self, position, mapbytes):
         '''
         Return a queue of targetpoints (x_mm, y_mm)
         '''
