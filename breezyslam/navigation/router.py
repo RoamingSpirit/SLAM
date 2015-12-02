b6c6e35e6a4179ebd76bb44570487a266e1228c8
RaspPiMQP <gjvincentelli@wpi.edu>
2015-12-02 22:27:58 +0000

    routing

---
+++ breezyslam/navigation/router.py
@@ -0,0 +1,19 @@
+'''
+Class for the routing.
+             
+author: Nils Bernhardt 
+'''
+
+
+class Router():
+
+    
+    def __init__(self):
+        return
+    
+    def getRoute(self):
+        '''
+        Return a queue of targetpoints (x_mm, y_mm)
+        '''
+        return
+       
