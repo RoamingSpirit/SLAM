c6b44d50cfc98ae4632bbf845372b909fe1f11bb
Hymerpatin <guille@vincentelli.org>
2015-12-04 14:51:01 -0500

    Neato class -> implemented so that sensor class can use it correctly. Imports NeatoReader.py

---
+++ breezyslam/sensor/Neato.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 02 12:13:15 2015
+
+@author: Guillermo the Great
+"""
+import XV_11_decoder3 as xv3
+from sensor import Sensor
+
+import NeatoReader as nr
+import math
+
+
+
+class NEATO(Sensor):
+    
+    viewangle = 360 #Neato LiDAR view in degrees
+    width = 360
+    distance_no_detection_mm = 3500 # max detection range, ===========>>>>check to make sure its correct
+    scan_rate_hz = 10 #todo find value
+    detectionMargin = 0 #pixels on the sites of the scans which should be ignored
+    offsetMillimeters = 50 #offset of the sensor to the center of the robot
+    
+    
+    def __init__(self, log = True):
+        self.log = log #Whether information should be saved in a log file
+        #if(log):
+         #   self.out = open('log', 'w')
+        self.sensor = nr.NeatoReader()
+        
+        Sensor.__init__(self, self.width, self.scan_rate_hz, self.viewangle, self.distance_no_detection_mm, self.detectionMargin, self.offsetMillimeters)
+        
+        
+
+    def scan(self):
+        data = self.sensor.scan()
+        return data
+       
+
+    
