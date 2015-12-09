2eb258021878ae301dc004a053c2f396af49867b
Lukas <lukas.brauckmann@gmail.com>
2015-12-09 12:01:49 -0500

    Added get_size().

---
+++ drone/vehicle.py
@@ -0,0 +1,39 @@
+'''
+vehilce.py: base class for any vehicle like turtlebot
+             
+author: Nils Bernhardt 
+'''
+
+import abc
+
+
+class Vehicle(object):
+
+    @abc.abstractmethod
+    def getOdometry(self):
+        """Return a tuple of odometry (dxy in mm,dthata in degree, dt in s)"""
+        return
+
+    @abc.abstractmethod
+    def initialize(self):
+        """Prepare"""
+        return
+
+    @abc.abstractmethod
+    def shutdown(self):
+        """Stop every running thread"""
+        return
+
+    @abc.abstractmethod
+    def move(self, cmd):
+        """
+        Move by dxy millimeters
+        :param cmd: Command.
+        """
+        return
+
+    @abc.abstractmethod
+    def getSize(self):
+        """Return size in meter"""
+        return
+
